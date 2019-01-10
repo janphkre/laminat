a97c8bf3c00c4c10ad83a6c5e9665a94fd28dd2a
Jan Phillip Kretzschmar <janphkre@gmx.de>
2019-01-11 00:24:05 +0100

    Removed unnecessary modules
    (cherry picked from commit 2773bb9)

--- pact-jvm-provider/src/main/kotlin/au/com/dius/pact/provider/unfiltered/Conversions.kt
+++
@@ -1,6 +0,0 @@
-package au.com.dius.pact.provider.unfiltered
-
-import com.ning.http.client.FluentCaseInsensitiveStringsMap
-
-fun toMap(headers: FluentCaseInsensitiveStringsMap) : Map<String, String> =
-  headers.mapValues { it.value.joinToString(separator = ", ") }
