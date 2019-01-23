f8133842eaed7d6a92b95a1ee29cb92f1f77a56a
Jan Phillip Kretzschmar <janphkre@gmx.de>
2019-01-23 12:57:17 +0100

    Fixed json object parsing

--- library/src/main/java/au/com/dius/pact/consumer/dsl/JSONObjectWrapper.kt
+++ library/src/main/java/au/com/dius/pact/consumer/dsl/JSONObjectWrapper.kt
@@ -2,9 +2,16 @@ package au.com.dius.pact.consumer.dsl
 
 import org.json.JSONObject
 
+/**
+ * This wrapper swallows "unhandled" JsonExceptions
+ */
 class JSONObjectWrapper {
 
     fun put(rootName: String, body: Any) {
+        if(body is JSONObjectWrapper) {
+            jsonObject.put(rootName, body.jsonObject)
+            return
+        }
         jsonObject.put(rootName, body)
     }
 
