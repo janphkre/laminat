c8fd5a02a77513d4e677508c00b2c87ed735e7f6
Jan Phillip Kretzschmar <janphkre@gmx.de>
2019-02-12 13:58:33 +0100

    deleted pact broker again

--- pact-broker/src/main/java/au/com/dius/pact/pactbroker/PactBrokerConsumer.kt
+++
@@ -1,6 +0,0 @@
-package au.com.dius.pact.pactbroker
-
-data class PactBrokerConsumer(val name: String,
-                              val source: String,
-                              val pactBrokerUrl: String,
-                              val pactFileAuthentication: List<String> = listOf())
--- pact-broker/src/main/java/au/com/dius/pact/pactbroker/PactResponse.kt
+++
@@ -1,6 +0,0 @@
-package au.com.dius.pact.pactbroker
-
-/**
- * Wraps the response for a Pact from the broker with the link data associated with the Pact document.
- */
-data class PactResponse(val pactFile: Any, val links: Map<String, Map<String, Any>>)
