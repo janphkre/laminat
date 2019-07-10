d2a00af5530223cda10937f33e78b8044c6056fd
Jan Phillip Kretzschmar <janphkre@gmx.de>
2019-07-10 18:25:16 +0200

    Fixed laminat with json cache

--- pact-consumer/src/main/java/au/com/dius/pact/external/Extensions.kt
+++ pact-consumer/src/main/java/au/com/dius/pact/external/Extensions.kt
@@ -1,22 +1,21 @@
 package au.com.dius.pact.external
 
-import android.util.LruCache
 import au.com.dius.pact.model.BasePact.Companion.jsonParser
 import com.google.gson.JsonElement
 import okhttp3.mockwebserver.RecordedRequest
+import java.util.WeakHashMap
 
-val jsonCache = LruCache<String, JsonElement>(20)
+val jsonCache = WeakHashMap<RecordedRequest, JsonElement>()
 
 @Synchronized
 fun RecordedRequest.parseBodyToJson(): JsonElement {
-    val inputBody = body.inputStream().use { it.reader().readText() }
-    val resolvedItem = jsonCache[inputBody]
+    val resolvedItem = jsonCache[this]
     if (resolvedItem != null) { return resolvedItem }
-    val element = body.inputStream().use { jsonParser.parse(inputBody) }
-    synchronized(jsonCache) {
-        val resolvedItemNew = jsonCache[inputBody]
+    synchronized(this) {
+        val resolvedItemNew = jsonCache[this]
         if (resolvedItemNew != null) { return resolvedItemNew }
-        jsonCache.put(inputBody, element)
+        val element = body.inputStream().use { jsonParser.parse(it.reader()) }
+        jsonCache.put(this, element)
+        return element
     }
-    return element
 }
