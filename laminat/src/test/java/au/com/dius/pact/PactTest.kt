14c829081debfb86758376f2fb20593da49d1c6b
Jan Phillip Kretzschmar <jan.kretzschmar@valtech.com>
2022-07-26 19:50:05 +0200

    minor cleanup adapting to windows

--- laminat/src/test/java/au/com/dius/pact/PactTest.kt
+++ laminat/src/test/java/au/com/dius/pact/PactTest.kt
@@ -20,7 +20,7 @@ import java.io.File
  */
 class PactTest {
 
-    private val expectedPact = "testconsumer:testproducer.json"
+    private val expectedPact = "testconsumer_testproducer.json"
 
     private val defaultRequestHeaders = hashMapOf(
         Pair("We", "will have to see about this!")
